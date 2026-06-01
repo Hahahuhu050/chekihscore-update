185864c8a39ad591b7830e44a0d8794c74271465
Hahahuhu050 <yogasadewa584@gmail.com>
2026-06-02 02:11:50 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
-const CACHE_NAME = 'sadewa-cekih-v1';
-const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('cekih-store').then((cache) => {
+      return cache.addAll(['/', '/index.html', '/app.js', '/logo.png', '/0.mp3']);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
